2b8a9ce98f9a5730762d9e03d6b17e47724c45e5
Susisu <susisu2413@yahoo.co.jp>
2026-09-20 18:46:45 +0900

    chore: install typescript and oxfmt

---
+++ oxfmt.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  experimentalOperatorPosition: "start",
+  htmlWhitespaceSensitivity: "css",
+  insertFinalNewline: true,
+  jsdoc: false,
+  jsxSingleQuote: false,
+  objectWrap: "preserve",
+  printWidth: 100,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  singleAttributePerLine: false,
+  singleQuote: false,
+  sortImports: true,
+  sortPackageJson: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+});
